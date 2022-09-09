8f045aa41af4e01aca97b7ecacd3d99ffda77658
MariaS-N <113188566+MariaS-N@users.noreply.github.com>
2022-09-09 14:35:26 +0100

    Add files via upload

---
+++ lotterynumbers.py
@@ -0,0 +1,44 @@
+import random
+
+one = print(random.randrange(1, 49))
+two = print(random.randrange(1, 49))
+three = print(random.randrange(1, 49))
+four = print(random.randrange(1, 49))
+five = print(random.randrange(1, 49))
+six = print(random.randrange(1, 49))
+bonus = print(random.randrange(1, 49))
+
+import random
+
+def one():
+  print(random.randrange(1, 49))
+  pass
+  
+def two():
+  print(random.randrange(1, 49))
+  pass
+def three():
+  print(random.randrange(1, 49))
+  pass
+def four ():
+  print(random.randrange(1, 49))
+  pass
+def five():
+  print(random.randrange(1, 49))
+  pass
+def six ():
+  print(random.randrange(1, 49))
+  pass
+def bonus ():
+  print(random.randrange(1, 49))
+  pass
+
+while (int(one)() not (int(two)()
+  one()
+  two()
+  three()
+  four()
+  five()
+  six()
+  bonus()
+
